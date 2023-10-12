096402c7c2e182576d431f3d958b4c5dec1ac886
Mel-TB <melinda.trami@live.fr>
2023-10-12 13:34:14 +0200

    Feature: Add Launches Model

---
+++ server/src/models/launches.models.js
@@ -0,0 +1,52 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: " Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-452b",
+  customer: ["NASA", "Mel"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    launch.flightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ["Mel", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+module.exports = {
+  launches,
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+};
