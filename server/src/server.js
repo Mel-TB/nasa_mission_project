13405fc398a4e8923b983c0b5759f13e212ccdb3
Mel-TB <melinda.trami@live.fr>
2023-10-12 14:22:45 +0200

    Features: Added Mongo/Mongoose Add Mongoose Schema for Launches & Planet

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,26 @@
-const http = require('http');
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
 
-const app = require('./app');
+const MONGO_URL =
+  "mongodb+srv://nasa-api:v0YanP369rC8hMab@nasacluster.pe54ibp.mongodb.net/?retryWrites=true&w=majority";
 
-const { loadPlanetsData } = require('./models/planets.models');
-const { start } = require('repl');
+const { loadPlanetsData } = require("./models/planets.models");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
